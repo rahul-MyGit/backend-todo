7f6da2fc8ae6bf637df1900307e3f079774fb241
Rahul <rahulhero93111@gmail.com>
2024-02-05 21:57:40 +0530

    added a button functionality

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react"
 
 export function CreateTodo() {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
     return <div>
-        
+
         <input style={{
             padding : 10,
             margin : 10
-        }} type="text" placeholder="title" /> <br />
+        }} type="text" placeholder="title" onChange={(e)=>{
+            const value = e.target.value;
+            setTitle(value);
+        }}/> <br />
         <input style={{
             padding : 10,
             margin : 10
-        }}  type="text" placeholder="description" /> <br />
+        }}  type="text" placeholder="description" onChange={(e)=>{
+            const value = e.target.value;
+            setDesc(value);
+        }}/> <br />
 
         <button style={{
             padding : 10,
             margin : 10
-        }}  type="button"> Add a todo</button>
+        }}  type="button" onClick={()=>{
+            fetch("http://localhost:3000/todo" , {
+                method: "POST",
+                body: JSON.stringify({
+                    title : title,
+                    desc : desc
+                }),
+                headers:{
+                    "Content-type": "application/json"
+                }
+            }) 
+                .then(async function(res){
+                    const json = await res.json();
+                    alert("Todo added");
+                })
+        }}> Add a todo</button>
 
     </div>
 }
